5c34b0307e53e6f3ee9274e1c48db8c48d04b02e
yishai <yishai5555@gmail.com>
2020-07-26 15:39:03 +0300

    add-css-modules

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './App.module.css';
+import classes from './App.module.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.test}
+  >
+    test css modules
     </div>
   );
 }
